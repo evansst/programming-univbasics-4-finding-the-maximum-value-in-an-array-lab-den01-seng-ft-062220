256eb992743f08dba9a10dc11bec0ac224cc57af
Sam Evans <evansst@icloud.com>
2020-06-15 23:43:19 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -3,7 +3,7 @@ def find_max_value(array)
   count = 0
   while count < array.size do
     maybe = array[count]
-    
+    binding.pry
     if array[count] >= maybe
       maybe = array[count]
     end
